67c4b8a2847f9e5903103760c8b51ac550b28fb4
Fabrizio Mettini <fabrizioamettini@gmail.com>
2023-12-06 09:46:00 -0300

    Add documentation and Style main.c

--- plancha_3/e3/sum.s
+++ plancha_3/e3/sum.s
@@ -1,14 +1,22 @@
+# Problem Set 03 - Exercise 03
+
+# void sum(float *a, float *b, int len);
+
+# Sum two arrays a and b of length len. The result is in array a.
+
 .global sum
 sum:
-    xorq %rcx, %rcx
+    xorl %ecx, %ecx                 # i = 0
 
-    sum_loop:
-        movss (%rdi, %rcx, 4), %xmm0
-        movss (%rsi, %rcx, 4), %xmm1
-        addss %xmm1, %xmm0
-        movss %xmm0, (%rdi, %rcx, 4)
+sum_loop:
+    movss (%rdi, %rcx, 4), %xmm0    # xmm0 = a[i]
+    movss (%rsi, %rcx, 4), %xmm1    # xmm1 = b[i]
+    addss %xmm1, %xmm0              # xmm0 = a[i] + b[i]
+    movss %xmm0, (%rdi, %rcx, 4)    # a[i] = a[i] + b[i]
 
-        incl %ecx
-        cmpl %edx, %ecx
-        jl sum_loop
+    incl %ecx                       # i++
+    cmpl %edx, %ecx                 # i < len ?
+    jl sum_loop                     # if true, go to new iteration
 
+    xorl %eax, %eax                 # return value is 0
+    ret
