343fca062219b75236a45ce871fbe21b055e3315
Fabrizio Mettini <fabrizioamettini@gmail.com>
2023-12-06 10:10:03 -0300

    Add documentation and Style main.c

--- plancha_3/e4/sum_simd.s
+++ plancha_3/e4/sum_simd.s
@@ -1,46 +1,48 @@
+# Problem Set 03 - Exercise 04
+
+# void sum_simd(float *a, float *b, int len);
+
+# Sum two arrays a and b of length len using SIMD instructions. The result is in array a.
+
 .global sum_simd
 sum_simd:
-    pushq %rbp
-    movq %rsp, %rbp
+    movl %edx, %eax                 # eax = len
+    xorq %rdx, %rdx                 # clean edx to make the division
 
-    movl %edx, %eax
-    xorq %rdx, %rdx 
+    movl $4, %r8d                   # r8d = 4
+    divl %r8d                       # edx = len % 8, eax = len / 8
 
-    movl $4, %r8d
-    divl %r8d
-
-    xorq %rcx, %rcx 
+    xorl %ecx, %ecx                 # i = 0
+    cmpl $0, %eax                   # len / 8 == 0 ?
+    je scalar_sum_loop              # if true, we won't do any simd instructions
 
 packed_sum_loop:
-    movaps (%rdi), %xmm0
-    movaps (%rsi), %xmm1
-    addps %xmm1, %xmm0
-    movaps %xmm0, (%rdi)
+    movaps (%rdi), %xmm0            # xmm0 = a[i+3]          | a[i+2]          | a[i+1]          | a[i]
+    movaps (%rsi), %xmm1            # xmm1 = b[i+3]          | b[i+2]          | b[i+1]          | b[i]
+    addps %xmm1, %xmm0              # xmm0 = a[i+3] + b[i+3] | a[i+2] + b[i+2] | a[i+1] + b[i+1] | a[i] + b[i]
+    movaps %xmm0, (%rdi)            # a[j] = a[j] + b[j], j := i..i+3
 
-    addq $16, %rdi
-    addq $16, %rsi
+    addq $16, %rdi                  # a = a + 16
+    addq $16, %rsi                  # b = b + 16
 
-    incl %ecx
-    cmpl %eax, %ecx
-    jl packed_sum_loop
+    incl %ecx                       # i++
+    cmpl %eax, %ecx                 # i < len / 8 ?
+    jl packed_sum_loop              # if true, go to new iteration
 
-    cmpl $0, %edx
-    je end
+    xorl %ecx, %ecx                 # i = 0
+    cmpl $0, %edx                   # len % 8 == 0?
+    je end                          # if true, we won't do any scalar instructions
 
-    xorq %rcx, %rcx 
-    
 scalar_sum_loop:
-    movss (%rdi, %rcx, 4), %xmm0
-    movss (%rsi, %rcx, 4), %xmm1
-    addss %xmm1, %xmm0
-    movss %xmm0, (%rdi, %rcx, 4)
+    movss (%rdi, %rcx, 4), %xmm0    # xmm0 = a[i]
+    movss (%rsi, %rcx, 4), %xmm1    # xmm1 = b[i]
+    addss %xmm1, %xmm0              # xmm0 = a[i] + b[i]
+    movss %xmm0, (%rdi, %rcx, 4)    # a[i] = a[i] + b[i]
 
-    incl %ecx
-    cmpl %edx, %ecx
-    jl scalar_sum_loop
+    incl %ecx                       # i++
+    cmpl %edx, %ecx                 # i < len ?
+    jl scalar_sum_loop              # if true, go to new iteration
     
 end:
-    movq %rbp, %rsp
-    popq %rbp
+    xorl %eax, %eax                 # return value is 0
     ret
-
